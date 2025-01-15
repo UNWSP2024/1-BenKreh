91f6ac193c59db39ebe56c7d47573b25f9784b6b
BenKreh <BenKrehbiel888@gmail.com>
2025-01-15 15:21:42 -0600

    Add files via upload

---
+++ Krehbiel Program Two.py
@@ -0,0 +1,9 @@
+def proverbs_22_29():
+    print(
+    'Do you see a man skillful in his work?\n'
+    'He will stand before kings;\n'
+    'he will not stand before obscure men.\n'
+    '       (Proverbs 22:29) '
+    )
+
+proverbs_22_29()
